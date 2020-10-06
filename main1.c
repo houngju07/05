8542dc6be2134abc3fa0f42d857bdecf5281f746
houngju07 <ohyoungju@sookmyung.ac.kr>
2020-10-06 16:16:18 +0900

    05.03
    05.03

--- main1.c
+++ main1.c
@@ -5,17 +5,19 @@
 
 int main(int argc, char *argv[]) {
 
-	int i;
+	int num=0;
+	char c;
 	
-	printf("정수 하나를 입력하시오 : ");
-	scanf("%i",&i);
+	printf("input a string: ");
 	
-	if(i>=0)
-    	printf("절대값은 %i입니다.",i);
-	
-	else
-		i= (-1)*i;
-        printf("절대값은 %i입니다.",i);
-	
+	while((c=getchar())!='\n')
+	{
+    	if(c>='0'&& c<='9')
+    	{
+			num++;
+		}
+	}
+    printf("the number of digitals is %i.\n",num);
+    	
 	return 0;
 }
