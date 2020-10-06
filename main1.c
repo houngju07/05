b37c8a82f50c63b57577d950e29ec23d72ae1ee0
houngju07 <ohyoungju@sookmyung.ac.kr>
2020-10-06 15:49:23 +0900

    05.02
    05.02

--- main1.c
+++ main1.c
@@ -10,14 +10,12 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오 : ");
 	scanf("%i",&i);
 	
-	if(i>0)
-    	printf("양수입니다.");
-	
-	else if(i<0) 
-        printf("음수입니다.");
+	if(i>=0)
+    	printf("절대값은 %i입니다.",i);
 	
 	else
-	    printf("0입니다.");
+		i= (-1)*i;
+        printf("절대값은 %i입니다.",i);
 	
 	return 0;
 }
